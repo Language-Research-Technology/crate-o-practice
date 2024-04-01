c71357ad5a38c352f78b51c92ea62436f6f36598
otili9890 <ocarm0809@gmail.com>
2024-04-02 10:57:03 +1100

    Updated help page with General Navigation

--- src/app/components/Help.vue
+++ src/app/components/Help.vue
@@ -1,6 +1,36 @@
 <script setup>
-import { ElRow, ElCol } from 'element-plus';
+import { ElRow, ElCol, ElTable, ElTableColumn } from 'element-plus';
 
+const tableData = [
+  {
+    metadata_group: 'About',
+    description: 'The core metadata for this RO-Crate and its subject matter.'
+  },
+  {
+    metadata_group: 'Related People, Orgs & Works',
+    description: 'The context for the creation of this RO-Crate; who made it, funded it etc.'
+  },
+  {
+    metadata_group: 'Structure',
+    description: 'How the parts of this RO-Crate relate, such as collection and object relationships.'
+  },
+  {
+    metadata_group: 'Provenance',
+    description: 'Detailed description of how entities were created, by whom and with which tools.'
+  },
+  {
+    metadata_group: 'Space & Time',
+    description: 'Where and when the data was collected; the times and places it mentions or describes.'
+  },
+  {
+    metadata_group: 'Software & Hardware',
+    description: 'For computer programs and execution environments that could be used to create data, have created data, or are being packaged and described.'
+  },
+  {
+    metadata_group: 'Others',
+    description: '[TODO: needs description]'
+  },
+]
 </script>
 <template>
   <el-row :gutter="20" class="p-2">
@@ -52,4 +82,36 @@ import { ElRow, ElCol } from 'element-plus';
     </el-col>
   </el-row>
 
+  <el-row :gutter="20" class="p-2">
+    <el-col :span="6" :xs="0" :sm="0" :md="4" :lg="6" :xl="8">
+      <div class="grid-content ep-bg-purple"/>
+    </el-col>
+    <el-col :span="18" :xs="24" :sm="24" :md="20" :lg="18" :xl="16">
+      <h2 class="mb-3 text-lg text-gray-500 md:text-xl dark:text-gray-400">General Navigation</h2>
+      <ul>
+        <li> - <strong>Selected Directory</strong> shows the directory or folder you currently have open.</li>
+        <li> - Below <strong>Selected Directory</strong>, you will see a home icon followed by a file path that indicates where you are located in the collection levels. This will change as you navigate to different sublevels.</li>
+        <li> - Depending on the metadata group, different types of metadata will be available:</li>
+      </ul>
+      <br>
+      <el-table :data="tableData" border style="width: 100%">
+        <el-table-column prop="metadata_group" label="Metadata Group" width="200" />
+        <el-table-column prop="description" label="Description" />
+      </el-table>
+      <br>
+      <p>There are several compulsory metadata fields required for each collection. To view these, go to [TODO: add link].</p>
+      <p>On the right-hand panel, there are some further options related to navigating and creating metadata entities:</p>
+      <ul>
+        <li><strong> - Create New Entity</strong>: [TODO: Needs description].</li>
+        <li><strong> - Links from</strong>: When located in a sub-group, select to view the parent group(s).</li>
+        <li><strong> - All Entities</strong>: Select to view all metadata associated with your collection.</li>
+        <li><strong> - Unlinked Entities</strong>: Select to view all metadata that currently is not linked to any properties.</li>
+      </ul>
+
+    </el-col>
+  </el-row>
 </template>
+
+<script lang="ts" setup>
+
+</script>
